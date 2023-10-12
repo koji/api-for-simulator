821bb666cf6d0430062306e9d164f1f8723ddbfc
koji <kk2796@nyu.edu>
2023-10-11 21:18:38 -0400

    add call_opentrons_simulate instead of using run_simulate method

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from opentrons.simulate import simulate, format_runlog, get_protocol_api
 import os
 import io
+import subprocess
 
 app = FastAPI()
 
@@ -44,10 +45,10 @@ class Protocol(BaseModel):
 def upload_protocol(protocol: Protocol):
     file_path = 'storage/' + protocol.name
     save_result = save_text_as_file(protocol.content, file_path)
-    
-    if save_result:
-        response = run_protocol_on_simulator(protocol.content)
-        
+
+    if type(save_result) == str:
+        # response = run_protocol_on_simulator(protocol.content)
+        response = call_opentrons_simulate(file_path)
         print('response', response)
         if response["status"] == "success":
             return {"protocol_name": protocol.name, "run_log": response['run_log']}
@@ -56,24 +57,35 @@ def upload_protocol(protocol: Protocol):
     else:
         return {"error_message": "something wrong while saving a protocol"}
 
+def call_opentrons_simulate(protocol_path: str):
+    command = f"opentrons_simulate {protocol_path}"
+
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    if result.returncode == 0:
+        # print("Command executed successfully! Output:")
+        # print(result.stdout)
+        return {"status": "success", "run_log": result.stdout}
+    else:
+        print("Command failed. Error message:")
+        # print(result.stderr)
+        return  {"status": "error", "error_message": result.stderr}
+
 
 def save_text_as_file(text, file_path):
+    base = os.path.splitext(file_path)[0]
+    ext = os.path.splitext(file_path)[1]
+    counter = 1
+
+    while os.path.exists(file_path):
+        file_path = base + "_" + str(counter) + ext
+        counter += 1
+
     try:
         with open(file_path, 'w') as file:
             file.write(text)
         print("Text saved successfully as", file_path)
-        return True
+        return file_path
     except Exception as e:
         print("An error occurred while saving the file:", str(e))
         return False
-
-
-def run_protocol_on_simulator(protocol_string):
-    protocol_file = io.StringIO(protocol_string)
-    try:
-        run_log = simulate(protocol_file)
-        run_log = format_runlog(run_log)
-        return {"status": "success", "run_log": run_log, "error_message": error_message}
-    except Exception as e:
-        error_message = f"An error occurred: {str(e)}"
-        return {"status": "error", "run_log": '', "error_message": error_message}
